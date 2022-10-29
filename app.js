0d31a621efd4f203c850c0942a1c4eb75d52f94f
Eduard <109389062+EddDevv@users.noreply.github.com>
2022-10-29 14:18:35 +0200

    main

--- app.js
+++ app.js
@@ -1,48 +1,75 @@
-const boxPicker = new iro.ColorPicker("#boxPicker", {
-  width: 320,
-  color: "rgb(255, 0, 0)",
-  borderWidth: 1,
-  boxHeight: 200,
-  layoutDirection: "vertical",
-  borderColor: "#fff",
-  layout: [
-    {
-      component: iro.ui.Box,
-    },
-    {
-      component: iro.ui.Slider,
-      options: {
-        id: 'hue-slider',
-        sliderType: 'hue'
-      }
-    }
-  ]
-});
+const copy = document.querySelector(".color-info");
+const tost = document.querySelector(".toast");
+const colorFormat = document.getElementById("color-format");
+const loader = document.querySelector(".loader");
 
-const colorFormat = document.getElementById("color-format")
+setTimeout(() => {
+  try {
+    const boxPicker = new iro.ColorPicker("#boxPicker", {
+      width: 320,
+      color: "rgb(255, 0, 0)",
+      borderWidth: 1,
+      boxHeight: 200,
+      layoutDirection: "vertical",
+      borderColor: "#fff",
+      layout: [
+        {
+          component: iro.ui.Box,
+        },
+        {
+          component: iro.ui.Slider,
+          options: {
+            id: "hue-slider",
+            sliderType: "hue",
+          },
+        },
+      ],
+    });
 
-document.body.style.backgroundColor = boxPicker.color.hexString
-colorFormat.innerHTML = boxPicker.color.hexString
+    document.body.style.backgroundColor = boxPicker.color.hexString;
+    colorFormat.innerHTML = boxPicker.color.hexString;
+    colorChangeText(colorFormat, boxPicker.color.hexString)
+    loader.style.display = "none";
+    copy.style.display = "flex";
 
-function colorChangeCallback(color) {
-  document.body.style.backgroundColor = color.hexString
-  colorFormat.innerHTML = color.hexString
-}
+    boxPicker.on("color:change", colorChangeBackground);
 
-boxPicker.on("color:change", colorChangeCallback);
-
-const copy = document.querySelector(".color-info ")
-const tost = document.querySelector(".toast")
+  } catch (e) {
+    console.log(e);
+  }
+}, 1500);
 
 copy.addEventListener("click", (e) => {
-  navigator.clipboard.writeText(e.target.firstElementChild.innerHTML)
-  .then(() => {
-    tost.classList.add("show")
-    setTimeout(() => {
-      tost.classList.remove("show")
-    }, 2000)
-  })
-  .catch(err => {
-    console.log('Something went wrong', err);
-  });
-})
+  navigator.clipboard
+    .writeText(e.target.firstElementChild.innerHTML)
+    .then(() => {
+      tost.classList.add("show");
+      tost.innerHTML = "Successfully copied!!!"
+      setTimeout(() => {
+        tost.classList.remove("show");
+      }, 1000);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+function colorChangeBackground(color) {
+  document.body.style.backgroundColor = color.hexString;
+  colorFormat.innerHTML = color.hexString;
+  colorChangeText(colorFormat ,color.hexString)
+}
+
+function colorChangeText(text, color) {
+  let icon = copy.querySelector("svg")
+  let luminance = chroma(color).luminance()
+  if (luminance > 0.5) {
+    text.style.color = "black"
+    icon.style.fill = "black"
+    tost.style.color = "black"
+  } else {
+    text.style.color = "white"
+    icon.style.fill = "white"
+    tost.style.color = "white"
+  }
+}
